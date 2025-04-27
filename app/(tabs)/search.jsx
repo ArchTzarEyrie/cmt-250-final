ce04e77a7341a388d24b655661f3fe87001f9387
ArchTzarEyrie <zackreiter@protonmail.com>
2025-04-27 17:31:31 -0400

    implement search tab

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -1,8 +1,37 @@
-import { Text } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
+import Task from '@/components/Task';
 
 const Search = () => {
+    const tasks = [
+        {
+            id: 1,
+            text: "Finish Final Project",
+            isComplete: false,
+            dueDate: new Date(),
+        },
+        {
+            id: 2,
+            text: "Create Server",
+            isComplete: false,
+            dueDate: new Date("May 1 2025"),
+        }
+    ];
+    const [searchTerm, setSearchTerm] = useState('');
     return (
-        <Text>Search</Text>
+        <View>
+            <Text>Search</Text>
+            <TextInput
+                onChangeText={setSearchTerm}
+                value={searchTerm}
+            />
+            <View>
+                {searchTerm !== '' && tasks.filter(task => {
+                    return task.text.toLowerCase().includes(searchTerm.toLowerCase());
+                }).map(task => <Task text={task.text} isComplete={task.isComplete} />)}
+            </View>
+        </View>
+        
     )
 }
 
